40b9700a01b87c84efe6f294562318b1f2301d6f
Rayyan Ashar <rayyan.spartan@gmail.com>
2026-08-05 15:23:45 -0700

    Finalization of M4, reading and pushing ot Excel, pending verification logic and wipe logic

--- M4.py
+++ M4.py
@@ -7,13 +7,6 @@ from M2 import calculate_utilization, read_samples
 
 path = config.EXCEL_PATH  # Use the EXCEL_PATH from config.py
 
-
-sample = read_samples(config.CSV_PATH)  # Read samples from the CSV file
-
-utilization_data = calculate_utilization(sample, config.ROBOTMODE_THRESHOLD)  # Calculate utilization data
-
-date = month_label = datetime.now().strftime("%B %Y")   # "May 2026" in local time  # noqa: DTZ005
-
 def write_to_excel(utilization_data, path, date):
 
     if path.exists():
@@ -26,19 +19,32 @@ def write_to_excel(utilization_data, path, date):
         ws.title = "Utilization Data"
 
         # Write headers
-        headers = ["Date","Powered On Time (s)", "Active Time (s)", "Idle Time (s)", "Utilization (%)"]
+        headers = ["Date","Powered On Time (hrs)", "Active Time (hrs)", "Idle Time (hrs)", "Utilization (%)"]
         ws.append(headers)
     # Write data
     data_row = [
-        utilization_data.powered_on_time,
+        date,
+        utilization_data.powered_on_time,  # Convert seconds to hours and round to 1 decimal place
         utilization_data.active_time,
-        utilization_data.idle_time,
+        utilization_data.idle_time,  
         utilization_data.utilization if utilization_data.utilization is not None else "N/A"
     ]
     ws.append(data_row)
 
     # Save the workbook
     wb.save(path)   
+
+
+if __name__ == "__main__":
+    
+    sample = read_samples(config.CSV_PATH)  # Read samples from the CSV file
+
+    utilization_data = calculate_utilization(sample, config.ROBOTMODE_THRESHOLD)  # Calculate utilization data
+
+    date = datetime.now().strftime("%B %Y")   # "May 2026" in local time  # noqa: DTZ005
+
+    write_to_excel(utilization_data, path, date)  # Write the data to the Excel file
+
 
 
 
--- config.py
+++ config.py
@@ -4,10 +4,11 @@ from pathlib import Path
 #File pathing variables, for XML recipe and CSV intermediary file
 ROBOTCONFIG = Path(r"C:\Users\localadmin\Desktop\RTDE Logger\RTDE-Logger-Code\RobotConfig.XML")
 CSV_PATH = Path(r"C:\Users\localadmin\Desktop\RTDE Logger\RTDE-Logger-Code\data.csv")
+EXCEL_PATH = Path(r"C:\Users\localadmin\Desktop\RTDE Logger\Excel Tests\RTDE Logger Test.xlsx")
 
 # RTDE connection parameters
 HOST = "127.0.0.1"
 PORT = 30004
 POLLING_FREQUENCY = 125 # in Hz, how often to poll the robot for data
-WRITING_FREQUENCY = 360 # Time interval between writes to the CSV file, corresponds to a frequency of every 6 minutes
+WRITING_FREQUENCY = 2 # Time interval between writes to the CSV file, corresponds to a frequency of every 6 minutes
 ROBOTMODE_THRESHOLD = 4  # robot_mode >= this counts as powered on; tune to change which states count
